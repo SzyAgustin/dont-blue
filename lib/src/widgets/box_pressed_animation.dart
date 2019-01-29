eb8f02153d0a37c9edc9d2491b6a95cceea80ef5
szyagustin <szyagustin@gmail.com>
2019-01-28 23:47:40 -0300

    Animation completed, with vibration

--- lib/src/widgets/box_pressed_animation.dart
+++ lib/src/widgets/box_pressed_animation.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'color_box.dart';
+import 'package:vibration/vibration.dart';
+import 'dart:async';
 
 class BoxPressedAnimation extends StatefulWidget {
   @override
@@ -10,6 +12,7 @@ class _BoxPressedAnimationState extends State<BoxPressedAnimation>
     with SingleTickerProviderStateMixin {
   Animation<double> _boxAnimation;
   AnimationController _boxController;
+  int counter = 0;
 
   @override
   void initState() {
@@ -17,16 +20,29 @@ class _BoxPressedAnimationState extends State<BoxPressedAnimation>
 
     _boxController = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 500),
+      duration: Duration(milliseconds: 25),
     );
 
-    _boxAnimation = Tween(begin: 200.0, end: 300.0).animate(CurvedAnimation(
+    _boxAnimation = Tween(begin: 200.0, end: 205.0).animate(CurvedAnimation(
       parent: _boxController,
-      curve: Curves.elasticInOut,
+      curve: Curves.easeIn,
     ));
 
     _boxController.addListener(() {
       setState(() {});
+    });
+
+    _boxAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _boxController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _boxController.forward();
+        counter++;
+        if (counter == 3) {
+          counter = 0;
+          _boxController.stop();
+        }
+      }
     });
   }
 
@@ -37,15 +53,15 @@ class _BoxPressedAnimationState extends State<BoxPressedAnimation>
   }
 
   onTap() {
-    if (_boxController.status == AnimationStatus.dismissed) {
-      _boxController.forward();
-    } else if (_boxController.status == AnimationStatus.completed) {
-      _boxController.reverse();
-      
-    }
+    executeAnimation();
+    // new Timer.periodic(Duration(milliseconds: 1000), (timer) {
+    //   print('one second passed');
+    // });
+  }
 
-    // _boxController.forward();
-    // _boxController.reverse();
+  executeAnimation(){
+    _boxController.forward();
+    Vibration.vibrate(duration: 75);
   }
 
   @override
@@ -56,6 +72,7 @@ class _BoxPressedAnimationState extends State<BoxPressedAnimation>
         //margin: EdgeInsets.only(top: _boxAnimation.value),
         height: _boxAnimation.value,
         width: _boxAnimation.value,
+        color: Colors.blue,
       ),
     );
   }
--- lib/src/widgets/color_box.dart
+++ lib/src/widgets/color_box.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 class ColorBox extends StatelessWidget {
   final width;
   final height;
+  final color;
 
-  ColorBox({this.width, this.height});
+  ColorBox({this.width, this.height, this.color});
 
   Widget build(BuildContext context) {
     return Container(
       width: width,
       height: width,
-      color: Colors.blue,
+      color: color,
     );
   }
 }
